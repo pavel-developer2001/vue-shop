fc672b6305a18737e42ad5ccdb39fb52438dced2
pavel-developer2001 <gaidukpavel18@gmail.com>
2021-02-15 13:39:52 +0300

    fix

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -7,6 +7,7 @@
             <CartItem />
             <CartItem />
         </div>
+        <h4 class='result'>Итог: <span class='result-money'>8000 рублей</span></h4>
     </div>
 </template>
 
@@ -29,5 +30,14 @@ export default {
         font-size: 30px;
         font-weight: bold;
         padding-bottom: 20px;
+    }
+    .result{
+        padding-top: 15px;
+        font-size: 20px;
+        font-weight: 500;
+    }
+    .result-money{
+        font-size: 22px;
+        font-weight: 700;
     }
 </style>
