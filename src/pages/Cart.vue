de879b2e92643d174e7a770d5eb249bb6ad9938e
pavel-developer2001 <gaidukpavel18@gmail.com>
2021-02-15 13:35:17 +0300

    add ui for cart page

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-        cart page
+    <div class='cart'>
+        <h3 class='cart__title'>Корзина</h3> 
+        <div class='cart__list'>
+            <CartItem />
+            <CartItem />
+            <CartItem />
+            <CartItem />
+        </div>
     </div>
 </template>
+
 <script>
+import CartItem from '../components/CartItem'
+
 export default {
-    
+    name: "Cart",
+    components: {
+        CartItem
+    }
 }
 </script>
-<style lang="">
-    
+
+<style scoped>
+    .cart{
+        padding: 25px 20px;
+    }
+    .cart__title{
+        font-size: 30px;
+        font-weight: bold;
+        padding-bottom: 20px;
+    }
 </style>
